73aaf429b9371fe557edfa94360abf68d7e7797d
deepank411 <deepank411@gmail.com>
2016-12-31 11:39:33 +0530

    localtrain page stable 1

--- javascript/menu.js
+++ javascript/menu.js
@@ -7,5 +7,3 @@ $(".menu").mousedown(function(){
 		$(".main.button").text("Close");
 	}
 })
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtZW51LmpzIl0sInNvdXJjZXNDb250ZW50IjpbIiQoXCIubWVudVwiKS5tb3VzZWRvd24oZnVuY3Rpb24oKXtcblx0JCh0aGlzKS50b2dnbGVDbGFzcyhcImNsb3NlZFwiKTtcblxuXHRpZigkKHRoaXMpLmhhc0NsYXNzKFwiY2xvc2VkXCIpKSB7XG5cdFx0JChcIi5tYWluLmJ1dHRvblwiKS50ZXh0KFwiTWVudVwiKTtcblx0fSBlbHNlIHtcblx0XHQkKFwiLm1haW4uYnV0dG9uXCIpLnRleHQoXCJDbG9zZVwiKTtcblx0fVxufSlcbiJdLCJmaWxlIjoibWVudS5qcyJ9
